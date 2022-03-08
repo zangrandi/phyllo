3e66de1399d72ce8588d9c88c0e164699815faf9
Augusto Zangrandi <augustozangrandi@gmail.com>
2022-03-08 14:16:00 -0300

    Updates to the client

--- lib/phyllo-wrapper/client.rb
+++ lib/phyllo-wrapper/client.rb
@@ -2,52 +2,65 @@ module PhylloWrapper
   class Client
     BASE_URL = "https://api.sandbox.getphyllo.com/v1"
 
-    attr_accessor :token
+    class << self
+      attr_accessor :configuration
 
-    def initialize(token)
-      @token = token
+      def configure
+        self.configuration ||= Configuration.new
+        yield(configuration)
+      end
+
+      # TODO: Extract to classes
+
+      def users(limit: 10, offset: 0)
+        get("users?limit=#{limit}&offset=#{offset}")
+      end
+
+      def accounts(user_id: nil, limit: 10, offset: 0)
+        get("accounts?user_id=#{user_id}&limit=#{limit}&offset=#{offset}")
+      end
+
+      def profiles(account_id: nil, limit: 10, offset: 0)
+        get("profiles?account_id=#{account_id}&limit=#{limit}&offset=#{offset}")
+      end
+
+      def contents(account_id: nil, limit: 10, offset: 0)
+        get("social/contents?account_id=#{account_id}&limit=#{limit}&offset=#{offset}")
+      end
+
+      def user_by_external_id(external_id)
+        get("users/external_id/#{external_id}")
+      end
+
+      def create_user(name:, external_id:)
+        post("users", { name: name, external_id: external_id }).parsed_response
+      end
+
+      def create_sdk_token(user_id:, products: ["IDENTITY", "ENGAGEMENT"])
+        post("sdk-tokens", { user_id: user_id, products: products }).parsed_response
+      end
+
+      private
+
+      def headers
+        {
+          'Content-Type' => 'application/json',
+          :Accepts => 'application/json',
+          :Authorization => "Basic #{PhylloWrapper::Client.configuration.api_key}"
+        }
+      end
+
+      def get(url)
+        HTTParty.get("#{BASE_URL}/#{url}", { headers: headers }).parsed_response["data"]
+      end
+
+      def post(url, body)
+        HTTParty.post("#{BASE_URL}/#{url}", { headers: headers , body: body.to_json})
+      end
     end
 
-    def users(limit: 10, offset: 0)
-      get("users?limit=#{limit}&offset=#{offset}")
-    end
-
-    def create_user(name: nil, external_id: nil)
-      post("users", { name: name, external_id: external_id }).parsed_response
-    end
-
-    def create_sdk_token(user_id:, products: ["IDENTITY", "ENGAGEMENT"])
-      post("sdk-tokens", { user_id: user_id, products: products }).parsed_response
-    end
-
-    def accounts_by_user_id(user_id, limit: 10, offset: 0)
-      get("accounts?user_id=#{user_id}&limit=#{limit}&offset=#{offset}")
-    end
-
-    def profiles_by_account_id(account_id, limit: 10, offset: 0)
-      get("profiles?account_id=#{account_id}&limit=#{limit}&offset=#{offset}")
-    end
-
-    def contents_by_account_id(account_id, limit: 10, offset: 0)
-      get("social/contents?account_id=#{account_id}&limit=#{limit}&offset=#{offset}")
-    end
-
-    private
-
-    def headers
-      {
-        'Content-Type' => 'application/json',
-        :Accepts => 'application/json',
-        :Authorization => "Basic #{token}"
-      }
-    end
-
-    def get(url)
-      HTTParty.get("#{BASE_URL}/#{url}", { headers: headers }).parsed_response["data"]
-    end
-
-    def post(url, body)
-      HTTParty.post("#{BASE_URL}/#{url}", { headers: headers , body: body.to_json})
+    class Configuration
+      attr_accessor :api_key
     end
   end
 end
